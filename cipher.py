7975448e0f94b8107596187f970103af2d169c36
cpt-nem0 <rohanya.76426@gmail.com>
2020-04-10 11:31:36 -0400

    add all the fuctions

--- cipher.py
+++ cipher.py
@@ -1,34 +1,48 @@
-#! /use/bin/python3
+#! /usr/bin/python3
+
 import base64
+import sys
 import hashlib
 
-# just the encryption key, could be anything
 key = b'AK8EwgCqe9-Y!YEhSwN9^Ft'
-
-# list of all base64 characters
-bs64_char = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz \
-0123456789+/']
+bs64_char = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=']
 
 
-def convert_input(string, type):
+def conversion(string, type):
     hash = hashlib.sha512(key).hexdigest()
     cipher = bs64_char[:]
 
     for c in hash:
-        char_int = int(c, 16)
-        position = 65 * (char_int / 15)
-
-        cipher.insert(0, cipher.pop(int(position)-1))
+        ciph_int = int(c, 16)
+        pos = 65 * (ciph_int / 15)
+        cipher.insert(0, cipher.pop(int(pos)-1))
         cipher = cipher[::-1]
 
-    cipher_box = {}
+    ciph_map = {}
 
     for i, c in enumerate(bs64_char):
-        cipher_box[c] = cipher[i]
+        ciph_map[c] = cipher[i]
 
     if type == 'd':
-        cipher_box = dict((v, k) for k, v in cipher_box.items())
+        ciph_map = dict((v, k) for k, v in ciph_map.items())
 
     for i, c in enumerate(string):
-        string[i] = string[c]
-        return ''.join(string)
+        string[i] = ciph_map[c]
+
+    return ''.join(string)
+
+
+def encryption(string):
+    string = [c for c in base64.b64encode(string.encode()).decode()]
+    return conversion(string, 'e')
+
+
+def decryption(string):
+    string = [c for c in string.strip()]
+    return base64.b64decode(conversion(string, 'd')).decode()
+
+
+if sys.argv[1] == '-e':
+    print(encryption(sys.stdin.read()))
+if sys.argv[1] == '-d':
+    print(decryption(sys.stdin.read()))
